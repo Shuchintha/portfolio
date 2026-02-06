1f286b06056b4e207197750dab2d45d048df0099
Shuchintha <shuchintha.srinivasa@gmail.com>
2026-02-06 09:34:06 -0800

    box shadow added for skills section

--- src/pages/Experience/Experience.tsx
+++ src/pages/Experience/Experience.tsx
@@ -1,5 +1,5 @@
 import { useRef, useState } from 'react';
-import { motion } from 'framer-motion';
+import { motion, AnimatePresence } from 'framer-motion';
 import { FiCalendar, FiChevronLeft, FiChevronRight } from 'react-icons/fi';
 import { experiences } from '../../data/portfolioData';
 import './Experience.scss';
